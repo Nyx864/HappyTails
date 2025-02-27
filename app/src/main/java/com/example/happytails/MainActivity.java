d00e5a6d82a4d461b29bbaaa5b23f3e480f60d40
Nyx864 <kebets19952@gmail.com>
2025-02-27 10:27:49 +0300

    add nav logic

--- app/src/main/java/com/example/happytails/MainActivity.java
+++ app/src/main/java/com/example/happytails/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.happytails;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.happytails.databinding.ActivityMainBinding;
 
@@ -28,7 +31,57 @@ public class MainActivity extends AppCompatActivity {
 
 
         binding.homeGroup.setOnClickListener(v -> {
-            Toast.makeText(this,"text", Toast.LENGTH_LONG).show();
+           setNavUnselected();
+            binding.homeImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.home_image_chosen));
+            binding.homeText.setTextColor(getColor(R.color.nav_selected));
         });
+
+        binding.tipsGroup.setOnClickListener(v -> {
+            setNavUnselected();
+            binding.tipsImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.tips_image_chosen));
+            binding.tipsText.setTextColor(getColor(R.color.nav_selected));
+        });
+
+        binding.petsGroup.setOnClickListener(v -> {
+            setNavUnselected();
+            binding.petsImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.pets_image_chosen));
+            binding.petsText.setTextColor(getColor(R.color.nav_selected));
+        });
+
+        binding.otherGroup.setOnClickListener(v -> {
+            setNavUnselected();
+            binding.otherImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.other_image_chosen));
+            binding.otherText.setTextColor(getColor(R.color.nav_selected));
+        });
+
+    }
+
+    private void setNavUnselected() {
+        binding.tipsImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.tips_image_unchosen));
+        binding.otherImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.other_image_unchosen));
+        binding.petsImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.pets_image_unchosen));
+        binding.homeImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.home_image_unchosen));
+        binding.tipsText.setTextColor(getColor(R.color.nav_unselected));
+        binding.otherText.setTextColor(getColor(R.color.nav_unselected));
+        binding.petsText.setTextColor(getColor(R.color.nav_unselected));
+        binding.homeText.setTextColor(getColor(R.color.nav_unselected));
+    }
+
+    private void init() {
+        binding.tipsImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.tips_image_unchosen));
+        binding.otherImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.other_image_unchosen));
+        binding.petsImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.pets_image_unchosen));
+        binding.homeImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.home_image_chosen));
+        binding.tipsText.setTextColor(getColor(R.color.nav_unselected));
+        binding.otherText.setTextColor(getColor(R.color.nav_unselected));
+        binding.petsText.setTextColor(getColor(R.color.nav_unselected));
+        binding.homeText.setTextColor(getColor(R.color.nav_selected));
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
