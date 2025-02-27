952ecdf2f03ecac33474f27b7848a27964e5e2aa
Nyx864 <kebets19952@gmail.com>
2025-02-27 09:36:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/happytails/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.happytails;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.happytails.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        binding.homeGroup.setOnClickListener(v -> {
+            Toast.makeText(this,"text", Toast.LENGTH_LONG).show();
+        });
+    }
+}
